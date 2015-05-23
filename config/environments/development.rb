1ff4ae4613d984e3da12593a9274ca639ae7f6a8
Markus Olsen <molsen13@gmail.com>
2015-05-23 15:37:47 -0600

    Setup redis store for caching in config/environments/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.cache_store = :redis_store, "redis://localhost:6379/0/keevah"
+  config.action_controller.perform_caching = true
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -11,7 +14,6 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
