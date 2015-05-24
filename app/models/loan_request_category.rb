916f7d7511ee8172c67d87f284cdfd729c628b96
Markus Olsen <molsen13@gmail.com>
2015-05-23 21:05:31 -0600

    Refreshing the cache for the most recently affected category when a new loanrequestcategory join relation is created

--- app/models/loan_request_category.rb
+++ app/models/loan_request_category.rb
@@ -1,4 +1,10 @@
 class LoanRequestCategory < ActiveRecord::Base
   belongs_to :category
   belongs_to :loan_request
+
+  after_save :update_most_recent_category
+
+  def update_most_recent_category
+    self.class.last.category.touch
+  end
 end
