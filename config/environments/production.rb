7e40bf3d4f4003927acfed21530d004956cee067
Markus Olsen <molsen13@gmail.com>
2015-05-26 17:37:50 -0600

    Added Memcachier on Heroku and setup production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,15 @@ Rails.application.configure do
     }
   }
 
+  config.cache_store = :dalli_store,
+  (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+  {:username => ENV["MEMCACHIER_USERNAME"],
+   :password => ENV["MEMCACHIER_PASSWORD"],
+   :failover => true,
+   :socket_timeout => 1.5,
+   :socket_failure_delay => 0.2
+  }
+
   config.cache_classes = true
 
   config.eager_load = true
@@ -33,9 +42,6 @@ Rails.application.configure do
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
