51b9f45e6c290bd87732f16ec07ab3c6b9b6636e
Markus Olsen <molsen13@gmail.com>
2015-05-29 09:16:50 -0600

    Removed comments from previous group in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,16 +7,12 @@ class UsersController < ApplicationController
   before_action :current_user, only: [:show, :edit, :update]
 
   def index
-    # @users = User.all
   end
 
   def new
-    # @user = User.new
   end
 
   def create
-    # @user = User.new(user_params)
-
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_path, notice: 'User created.'
